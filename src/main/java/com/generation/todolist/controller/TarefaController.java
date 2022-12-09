90e19e5caef1dcc9ec25158d5bcc974cabaec922
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-09 17:37:16 -0300

    Implementado o método buscar por nome.

--- src/main/java/com/generation/todolist/controller/TarefaController.java
+++ src/main/java/com/generation/todolist/controller/TarefaController.java
@@ -4,10 +4,7 @@ import com.generation.todolist.model.Tarefa;
 import com.generation.todolist.repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class TarefaController {
     @GetMapping("/listar")
     public ResponseEntity<List<Tarefa>> getAll(){
         return ResponseEntity.ok(tarefaRepository.findAll());
+    }
+
+    @GetMapping("/listar/{nome}")
+    public ResponseEntity <List<Tarefa>> getByNome(@PathVariable String nome){
+        return ResponseEntity.ok(tarefaRepository.findAllByNomeContainingIgnoreCase(nome));
     }
 }
