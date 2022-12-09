2d6f26cac4f57c817f7093ee2a6dff9c6f2769ff
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-09 17:12:04 -0300

    Criada assinatura para o método buscar por nome.

--- src/main/java/com/generation/todolist/repository/TarefaRepository.java
+++ src/main/java/com/generation/todolist/repository/TarefaRepository.java
@@ -1,11 +1,14 @@
 package com.generation.todolist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.todolist.model.Tarefa;
 
+import java.util.List;
+
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long>{
-
+    public List<Tarefa> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
