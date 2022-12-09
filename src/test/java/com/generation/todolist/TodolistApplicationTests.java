f297206b9b48ade7cdbf691f7ec206d4836d619c
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-09 18:11:50 -0300

    Criados testes para atualizar e cadastrar tarefas.

--- src/test/java/com/generation/todolist/TodolistApplicationTests.java
+++ src/test/java/com/generation/todolist/TodolistApplicationTests.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,7 +42,21 @@ class TodolistApplicationTests {
 		ResponseEntity<String> resposta= testRestTemplate
 				.exchange("/tarefas/listar/"+ procuraTarefa.getNome(), HttpMethod.GET, null, String.class);
 		assertEquals(HttpStatus.OK, resposta.getStatusCode());
+	}
+	@Test
+	@DisplayName("Atualizar tarefa")
+	public void deveAtualizarUmaTarefa(){
+		Optional<Tarefa> tarefaCadastrada= tarefaRepository.save();
+	}
 
+	@Test
+	@DisplayName("Cadastrar Tarefa")
+	public void deveCadastrarUmaTarefa(){
+		HttpEntity<Tarefa> corpoRequisicao= new ResponseEntity<>(new Tarefa(0L, "tarefa 2", "tarefa número 2", "Moises", LocalDate.now(), true));
+		ResponseEntity<Tarefa> corpoResposta = testRestTemplate
+			.exchange("/tarefas/cadastrar", HttpMethod.POST, corpoRequisicao, Tarefa.class);
+		assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+		assertEquals(corpoRequisicao.getBody().getNome(), corpoResposta.getBody().getNome());
 	}
 
 }
